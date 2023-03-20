c377c6e7c92a0936eea17b2fa4b2626e989f8ac1
Mauro Sala <mauro.sala@gmail.com>
2023-03-20 21:38:13 +0100

    ext

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
+import glob
 
 name = os.environ.get("INPUT_NAME")
 repo = os.environ.get("INPUT_REPO")
+ext = os.environ.get("INPUT_EXT")
 
 print("Hello " + name + ", greetings from " + repo)
+
+
+files = glob.glob("." + '/**/*' + ext, recursive=True)
+
+print("Wow! There are " + str(len(files)) + " files with the extension " + ext + " in this repo!")
